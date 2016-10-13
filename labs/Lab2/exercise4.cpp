99b44b35367f8235815e465092248a42a07dff7f
Kerrie Ann Sutton <KERRIEANN.SUTTON.2015@mumail.ie>
2016-10-13 17:37:46 +0100

    Added functional code to concatinate two strings

--- labs/Lab2/exercise4.cpp
+++ labs/Lab2/exercise4.cpp
@@ -15,15 +15,32 @@ int my_string_len(char str[]){
 }
 
 void my_string_cat(char dest[], char src[], int dest_size){
-
+	int smallsize = my_string_len(src);
+	int taken = my_string_len(dest);
+	int j = 0;
+	if(dest_size-(smallsize+taken)>=0){
+		for(int i = taken;i<dest_size;++i){
+			dest[i]=src[j];
+			++j;
+		}
+		j=0;
+		while(dest[j]!='\0'){
+			cout<<dest[j];
+			j++;
+		}
+		cout<<endl;
+	}
+	else{
+		cout<<"You don goofed"<<endl;
+	}
 }
 
 int main(){
 	char fun[] = "hello";
-	char final[20] = "world";
-	int size = 0;
-	size = my_string_len(fun);
-	cout<< "Size of first string: \n"<<size<<endl;
+	char final[15] = "world";
+	int size = 15; //size of dest string
+	my_string_cat(final, fun, size);
+	
 	
 
 
