2c90a6e5bbaad1de77b733dfd7d40bb740876311
Kerrie Ann Sutton <KERRIEANN.SUTTON.2015@mumail.ie>
2016-10-20 12:48:15 +0100

    Added comments to code

--- labs/Lab3/exercise4.cpp
+++ labs/Lab3/exercise4.cpp
@@ -1,5 +1,6 @@
 /* Kerrie-Ann Sutton
 14378311
+Linked list structure using pointers
 */
 #include <iostream>
 #include <string>
@@ -11,21 +12,21 @@ struct customer{
 	customer *next;
 };
 
-void print_customers(customer &head)
+void print_customers(customer &head) //method to print list
 {
-	customer *cur = &head;
+	customer *cur = &head; //sets current to the head of the list
 	while(cur != NULL)
 	{
-		cout<<cur->name<<endl;
-		cur = cur->next;
+		cout<<cur->name<<endl; //dereferences pointer to print value of name
+		cur = cur->next; //sets current to the next address in list
 	}
 }
 
 int main(){
 	customer customer1, customer2, customer3;
-	customer1.next=&customer2;
+	customer1.next=&customer2; //creates a link between 1 and 2
 	customer2.next=&customer3;
-	customer3.next=NULL;
+	customer3.next=NULL; //ends the list
 	customer1.name="Jack";
 	customer2.name="Jane";
 	customer3.name="Joe";
