0fd8a19d16bf1d93eddf06702b84654584dbf265
Kerrie Ann Sutton <KERRIEANN.SUTTON.2015@mumail.ie>
2016-10-20 12:44:08 +0100

    Added comments to code

--- labs/Lab3/exercise1.cpp
+++ labs/Lab3/exercise1.cpp
@@ -8,14 +8,14 @@ using namespace std;
 int main(){
 	cout<<"Please input the length of the sequence: "<<endl;
 	int num=0;
-	cin>>num;
-	double *arr = new double[num]; //initialises 
+	cin>>num; //user inputs length of array
+	double *arr = new double[num]; //initialises array on heap
 	for(int i = 0;i<num;++i){
-		cin>>arr[i];
+		cin>>arr[i]; //adds input values to array
 	}
 	for(int k=num-1;k>=0;--k){
-		cout<<arr[k]<<"\t";
+		cout<<arr[k]<<"\t"; //prints array in reverse order
 	}
 	cout<<endl;
-	delete arr;
+	delete arr; //deletes memory to prevent memory leak
 }
