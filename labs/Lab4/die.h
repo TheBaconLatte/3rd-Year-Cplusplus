65c6d3027555367f492654c327b931535bba133c
Kerrie Ann Sutton <KERRIEANN.SUTTON.2015@mumail.ie>
2016-11-07 15:51:03 +0000

    Added what I have so far for die.h of Lab4 exercise1

---
+++ labs/Lab4/die.h
@@ -0,0 +1,17 @@
+/*Kerrie-Ann Sutton
+14378311
+Declares functions in header file
+*/
+#include <iostream>
+#include <cstdlib> //for random
+#include <ctime>
+
+using namespace std;
+
+class die{
+	public:
+		int roll();
+		int getValue();
+		int get Numsides();
+}
+
